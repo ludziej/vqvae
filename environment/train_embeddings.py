d40196e99af4927219294e95373c22206b435514
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2021-10-24 13:24:34 +0200

    server ddp changes

--- environment/train_embeddings.py
+++ environment/train_embeddings.py
@@ -53,6 +53,6 @@ def train(batch_size, sample_len, num_workers,  data_depth, sr, gpus, test_path=
 
     checkpoint_callback = ModelCheckpoint(dirpath='generated/best_checkpoint/', filename='best_model', monitor="val_loss")
     tb_logger = pl_loggers.TensorBoardLogger("generated/logs/")
-    trainer = Trainer(gpus=gpus, log_every_n_steps=1, logger=tb_logger, default_root_dir="generated/checkpoints",
-                      callbacks=[checkpoint_callback], max_epochs=10000)
+    trainer = Trainer(gpus=gpus, log_every_n_steps=1, logger=tb_logger, default_root_dir="generated/checkpoints", accelerator='dp',
+                      callbacks=[checkpoint_callback], max_epochs=50000)
     trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=test_dataloader)
