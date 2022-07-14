c3ee1afdd468b93c7199999987daa48809c5e1c9
jan ludziejewski <jahulas@gmail.com>
2022-07-14 19:57:40 +0200

    fix for error

--- data_processing/normalization.py
+++ data_processing/normalization.py
@@ -3,6 +3,6 @@ import torch
 
 def rms_normalize(sig, rms_level=0):
     r = 10 ** (rms_level / 10.0)
-    a = torch.sqrt((len(sig) * r ** 2) / torch.sum(sig ** 2))
+    a = torch.sqrt(1e-7 + (len(sig) * r ** 2) / torch.sum(sig ** 2))
     y = sig * a
     return y
