2e41723cb7cdddf733a8382bdadc62e82b4752c2
jan ludziejewski <jahulas@gmail.com>
2022-07-14 21:59:08 +0200

    normalization less loud

--- data_processing/normalization.py
+++ data_processing/normalization.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def rms_normalize(sig, rms_level=0):
+def rms_normalize(sig, rms_level=-18):
     r = 10 ** (rms_level / 10.0)
     a = torch.sqrt(1e-7 + (len(sig) * r ** 2) / torch.sum(sig ** 2))
     y = sig * a
