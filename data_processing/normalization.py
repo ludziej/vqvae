e17b4f381e6cd8c06e40d523c9944e7c35c667a6
jan ludziejewski <jahulas@gmail.com>
2022-07-20 23:20:40 +0200

    removed debug

--- data_processing/normalization.py
+++ data_processing/normalization.py
@@ -3,7 +3,6 @@ import torch
 
 def rms_normalize(sig, rms_level=-18, eps=1e-7):
     if torch.mean(torch.abs(sig)) < eps:
-        print("skipping because norm bad")
         return sig
     r = 10 ** (rms_level / 10.0)
     a = torch.sqrt(1e-7 + (len(sig) * r ** 2) / torch.sum(sig ** 2))
