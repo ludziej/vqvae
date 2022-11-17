86ea346e0ee8c9541894576687fbc708def17506
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2022-11-17 18:33:34 +0100

    added nnAudio

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -1,4 +1,4 @@
 conda create --name waveformer python=3.10 pytorch torchvision torchaudio cudatoolkit=11.6 pytorch-lightning tqdm jsonargparse librosa -c pytorch -c nvidia -c conda-forge
 conda activate waveformer 
-pip3 install performer_pytorch audiofile
+pip3 install performer_pytorch audiofile nnAudio
 
