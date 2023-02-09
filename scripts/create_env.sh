13f1a8ddb70ab79f8d89f0ab72f545ca7bedeafe
Jan Ludziejewski <ludziej@bruce.mimuw.edu.pl>
2023-02-09 14:36:22 +0100

    pandas in create env

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -1,4 +1,4 @@
 conda create --name waveformer python=3.10 pytorch torchvision torchaudio cudatoolkit=11.6 pytorch-lightning tqdm jsonargparse librosa cudatoolkit-dev -c pytorch -c nvidia -c conda-forge 
 conda activate waveformer 
-pip3 install pytorch-fast-transformers performer_pytorch audiofile nnAudio
+pip3 install pytorch-fast-transformers performer_pytorch audiofile nnAudio pandas
 
