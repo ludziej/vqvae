5f36d5dc57f4b224f7c8e91c1774c07092a867b4
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2023-03-29 16:30:18 +0200

    new training params

--- train_diffusion.sh
+++ train_diffusion.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python3 run_training.py --gpus=[0,1] --config=config_big --model=diffusion --compressor=big_vae \
-  --train_path=resources/fma_wav --diffusion.main_dir=generated/models/big_diffusion/fma_cond/ \
+	--train_path=resources/fma_wav --diffusion.main_dir=generated/models/big_diffusion/fma_cond_3/ \
 	--compressor.batch_size=10 --compressor.num_workers=4 --diffusion.prep_chunks=2 \
-	--diffusion.condition_params.style_cond_size=256
