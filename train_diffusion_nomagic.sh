a7f2e144e8265cfe8ac75d73a8bb71aadf9314d3
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2023-04-07 20:51:51 +0200

    new train params

--- train_diffusion_nomagic.sh
+++ train_diffusion_nomagic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 run_training.py --gpus=[0,1,2,3] --config=config_big --model=diffusion --compressor=big_vae \
 	--train_path=/scidata/mimuw-jan-ludziejewski/fma_full_wav \
-	--diffusion.main_dir=generated/models/big_diffusion/fma_nonorm/ --diffusion.neptune_run_id="" \
+	--diffusion.main_dir=generated/models/big_diffusion/fma_unet_deep/ --diffusion.neptune_run_id="" \
 	--compressor.batch_size=18 --compressor.num_workers=8 --diffusion.prep_chunks=2 \
 
