ac48595e129b9bc7e1518761a7e0909234245192
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2023-04-20 14:57:49 +0200

    fixed train params

--- train_diffusion_nomagic.sh
+++ train_diffusion_nomagic.sh
@@ -4,6 +4,6 @@ python3 run_training.py --gpus=[0,1,2,3] --config=config_big --model=diffusion -
 	--diffusion.main_dir=generated/models/big_diffusion/fma_unet_big_tok/ \
 	--tags=[A100,attn_heads_8,max_width_2048,min_width_64,s_width_8,lr_decay_2k,block_depth_4] \
 	--diffusion.autenc_params.attn_heads=8 --diffusion.autenc_params.width=8 --diffusion.autenc_params.max_width=2048 --diffusion.autenc_params.min_width=64 --diffusion.autenc_params.emb_width=2048 \
-	--diffusion.opt_params.lr_decay=2000 --diffusion.autenc_params.depth=4 \	
+	--diffusion.opt_params.lr_decay=2000 --diffusion.autenc_params.depth=4 --diffusion.autenc_params.width=8 \
 	--compressor.batch_size=18 --compressor.num_workers=8 --diffusion.prep_chunks=2 \
 
